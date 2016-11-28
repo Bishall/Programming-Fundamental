5bc113b6ed878be0e3259cac073ded2847d5df48
Bishall <bishal.kc2055@gmail.com>
2016-11-28 10:55:25 +0545

    Add files via upload

---
+++ marksheett.c
@@ -0,0 +1,45 @@
+/*
+Student: Bishal K.C
+Subject: Programming Fundamental
+Lab Sheet: 06
+Program: Relational Operators
+Date: 2016/11/28
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+	int a,b,c,d,e,tot,ave,per;
+
+	printf("Enter the marks of the first subject");
+	scanf("%d",&a);
+
+	print("Enter the marks of the second subject");
+	scanf("%d",&b);
+
+	printf("Enter the marks of the third subject");
+	scanf("%d",&c);
+
+	printf("Enter the marks of the fourth subject");
+	scanf("%d"&d);
+
+	printf("Enter the marks of the fifth subject");
+	scanf("%d"&e);
+
+	tot=a+b+c+e+f;
+	ave=tot/5;
+	per=ave*100;
+
+	printf("The total marks obtained: %d",tot);
+
+	printf("The average marks is:%d",ave);
+
+	printf("The percentage obtained:%d",per);
+
+
+
+
+	getch();
+	return (0);
+}
