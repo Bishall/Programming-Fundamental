88909000b5f0d2a704e5448a94b2a13b4079be3d
Bishall <bishal.kc2055@gmail.com>
2016-11-30 12:23:54 +0545

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,29 @@
+/*
+Student Name: Bishal K.C
+Subject: Programming Fundamental
+Lab Sheet: 08
+Program: WAP to use assignment operators
+Date: 2016/11/30
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+int a,b;
+
+printf("Enter First No.:");
+scanf("%d",&a);
+
+printf("Enter Second No.:");
+scanf("%d",&b);
+
+a+=b;
+
+printf("+=%d",a);
+
+getch();
+return(0);
+}
+
+
