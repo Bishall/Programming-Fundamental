2c0313b661f4e2314812f3196c8f0225a55b3e7a
Bishall <bishal.kc2055@gmail.com>
2016-12-12 11:48:37 +0545

    Add files via upload

---
+++ odd,even.c
@@ -0,0 +1,32 @@
+/*
+Student: Bishal K.C
+Roll no.: 10
+Subject: Programming Fundamental
+Lab sheet: 11
+Program: WAP to odd/even number
+Date: 2016/12/12
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+ int main(){
+    int a;
+
+    printf("Enter the any number:");
+    scanf("%d",&a);
+
+    if(a%2==0){
+        printf("The value is even:%d",a);
+
+    }
+    else{
+        printf("The value is odd:%d",a);
+        }
+
+
+
+
+getch();
+return(0);
+}
