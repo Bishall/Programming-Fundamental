eaf960a1130fb69e25e6c09633221d700993580d
Bishall <bishal.kc2055@gmail.com>
2016-12-12 12:04:19 +0545

    Add files via upload

---
+++ odd,even using ternary operators.c
@@ -0,0 +1,26 @@
+/*
+Student: Bishal K.C
+Roll no.: 10
+Subject: Programming Fundamental
+Lab sheet: 11
+Program: WAP to find largest of the three numbers
+Date: 2016/12/12
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a;
+
+    printf("Enter any number:\n");
+    scanf("%d",a);
+
+    (a%2==0)?printf("The number is even"):printf("The number is odd");
+
+
+
+
+getch();
+return(0);
+}
